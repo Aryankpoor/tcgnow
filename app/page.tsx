f5ed5c8ef6eb6a0c628746e0d917670fd7394ea9
ARYAN KAPOOR <kapooraryan0419@gmail.com>
2025-09-24 00:48:13 +0530

    added navbar basic

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+
+import Navbar from "@/components/Navbar";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+
+   
+      <main className="pt-16">
+        {/* Your main content goes here */}
+      </main>
+
+      <footer>
+        <Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+      </footer>
+    </div>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,93 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
+export default function Navbar() {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-background border-b mt-0">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Topps Match Attax</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Match Attax 2025</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 24/25 Extra</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 24/25</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 23/24 Extra</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 23/24</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 22/23 Extra</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 22/23</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 21/22 Extra</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 21/22</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 20/21 Extra</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 20/21</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 19/20 Extra</NavigationMenuLink>
+              <NavigationMenuLink>Match Attax 19/20</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Topps Cricket Attax</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Cricket Attax 'THE HUNDRED' 2022</NavigationMenuLink>
+              <NavigationMenuLink>Cricket Attax 'THE HUNDRED' 2021</NavigationMenuLink>
+              <NavigationMenuLink>Cricket Attax 2017</NavigationMenuLink>
+              <NavigationMenuLink>Cricket Attax 2016</NavigationMenuLink>
+              <NavigationMenuLink>Cricket Attax 2015</NavigationMenuLink>
+              <NavigationMenuLink>Cricket Attax 2014</NavigationMenuLink>
+              <NavigationMenuLink>Cricket Attax 2013</NavigationMenuLink>
+              <NavigationMenuLink>Cricket Attax 2012</NavigationMenuLink>
+              <NavigationMenuLink>Cricket Attax 2011</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Topps Slam Attax</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Slam Attax Universe</NavigationMenuLink>
+              <NavigationMenuLink>Slam Attax Live</NavigationMenuLink>
+              <NavigationMenuLink>Slam Attax Superstars</NavigationMenuLink>
+              <NavigationMenuLink>Slam Attax Rebellion</NavigationMenuLink>
+              <NavigationMenuLink>Slam Attax Rumble</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Topps Now</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Topps Chrome</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Premier League 2026 Chrome</NavigationMenuLink>
+            <NavigationMenuLink>Match Attax 21/22 Chrome</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Other Topps Collections</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Topps Premier League 2026</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>By Sport</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Cricket</NavigationMenuLink>
+            <NavigationMenuLink>Football</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenu> 
+    </header>
+  );
+}
